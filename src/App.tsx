4a19f1d60bd041d8be2c99866ca47d5aef009fbf
sajedakhdair <sajeda.khdair@gmail.com>
2020-04-16 19:08:57 +0300

    add Login Page as a style only

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,12 @@
 import React from "react";
 import LogInComponent from "../src/components/LoginComponent";
+import LoginPage from "../src/pages/LoginPage";
 import "./App.css";
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <LogInComponent
-          bgColorForForm="#999"
-          bgColorForTextField="#FFF"
-          bgColorForButton="#4d70fe"
-        />
+        <LoginPage />
       </header>
     </div>
   );
