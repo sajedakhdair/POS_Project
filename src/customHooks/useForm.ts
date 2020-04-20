486fee3f53e92976a5dd374fb28c03031fdbad04
sajedakhdair <sajeda.khdair@gmail.com>
2020-04-20 11:15:58 +0300

    modify function handleSubmit, arrange the codes and add types file

--- src/customHooks/useForm.ts
+++ src/customHooks/useForm.ts
@@ -1,13 +1,15 @@
-import { useState, useEffect } from "react"
+import { useState } from "react"
 import { useHistory } from "react-router-dom";
-
+import { errorsProps } from "../types";
+import { checkValues } from "../utils"
+import { isThereAnyError } from "../utils";
+import { fetchSubmitLogin } from "../apis/fetchSubmitLogin"
 const useForm = () => {
     const [userInformation, setValues] = useState({ userName: "", password: "" });
     const [errors, setErrors] = useState({
         flagForuserNameErrors: false, userNameError: "",
         flagForPasswordError: false, passwordError: ""
     });
-    const [isSubmitting, setIsSubmitting] = useState(false);
     const history = useHistory();
 
     const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
@@ -15,51 +17,16 @@ const useForm = () => {
         setValues({ ...userInformation, [name]: value });
     };
 
-    const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+    const handleSubmit = async (event: React.FormEvent<HTMLFormElement>) => {
         event.preventDefault();
-        setErrors(checkValues());
-        setIsSubmitting(true);
+        const errors: errorsProps = checkValues(userInformation);
+        const hasErrors: boolean = isThereAnyError(errors);
+        if (!hasErrors) {
+            if (await fetchSubmitLogin(userInformation))
+                history.push("/MainPage");
+        }
+        else setErrors(errors);
     };
-
-    const checkValues = () => {
-        let errors = {
-            flagForuserNameErrors: false, userNameError: "",
-            flagForPasswordError: false, passwordError: ""
-        }
-        const password = userInformation.password;
-        const userName = userInformation.userName;
-        const charsAndNumer: RegExp = /^(?=.*[a-zA-Z])(?=.*[0-9])[a-zA-Z0-9]+$/;
-
-        if (userName.length === 0) {
-            errors.userNameError = "userName is required"
-            errors.flagForuserNameErrors = true;
-        }
-        if (password.length === 0) {
-            errors.passwordError = "password is required"
-            errors.flagForPasswordError = true;
-        }
-        else if (password.length < 4) {
-            errors.passwordError = "password needs to be at least 4 char"
-            errors.flagForPasswordError = true;
-        }
-        else if (!password.match(charsAndNumer)) {
-            errors.passwordError = "Password must contain chars and numbers"
-            errors.flagForPasswordError = true;
-        }
-        return ((errors));
-    }
-
-    useEffect(() => {
-        if (errors.userNameError === "" && errors.passwordError === "" && isSubmitting) {
-            fetch(`http://localhost:3001/users?username=${userInformation.userName}&password=${userInformation.password}`)
-                .then((response) => response.json())
-                .then((data) => {
-                    if (data.length !== 0) {
-                        history.push("/MainPage");
-                    }
-                })
-        }
-    }, [errors])
 
     const returnValues = { handleChange, handleSubmit, userInformation, errors }
     return returnValues
---
+++ src/types.ts
@@ -0,0 +1,11 @@
+
+export interface userInformationProps {
+    userName: string;
+    password: string;
+}
+export interface errorsProps {
+    flagForuserNameErrors: boolean;
+    userNameError: string;
+    flagForPasswordError: boolean;
+    passwordError: string;
+}
