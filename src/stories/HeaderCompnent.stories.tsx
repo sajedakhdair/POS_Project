ce0d66ddac816884a05cefbff3ccc1d5d2a07a9a
sajedakhdair <sajeda.khdair@gmail.com>
2020-04-23 13:41:45 +0300

    add the header component to the storybook

---
+++ src/stories/HeaderCompnent.stories.tsx
@@ -0,0 +1,6 @@
+import React from "react";
+import { storiesOf } from "@storybook/react";
+import HeaderComponent from "../components/HeaderComponent";
+storiesOf("HeaderComponent", module).add("HeaderComponent", () => (
+  <HeaderComponent />
+));
