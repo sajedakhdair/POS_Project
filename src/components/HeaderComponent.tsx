9d60484b362b936c7151a42127eb92418ec765da
sajedakhdair <sajeda.khdair@gmail.com>
2020-04-26 15:40:41 +0300

    build the right part of the header by using storybook

--- src/components/HeaderComponent.tsx
+++ src/components/HeaderComponent.tsx
@@ -4,7 +4,7 @@ import AppBar from "@material-ui/core/AppBar";
 import Toolbar from "@material-ui/core/Toolbar";
 import { withStyles, WithStyles } from "@material-ui/core/styles";
 import livePOSImg from "../imgs/livePOS.png";
-import { Box, Link } from "@material-ui/core";
+import { Box, Link, Avatar, Button, Typography } from "@material-ui/core";
 import PaymentIcon from "@material-ui/icons/Payment"; //pos
 import BookmarkIcon from "@material-ui/icons/Bookmark"; //cat
 import SettingsIcon from "@material-ui/icons/Settings";
@@ -14,6 +14,10 @@ import LocalMallIcon from "@material-ui/icons/LocalMall";
 import ReceiptIcon from "@material-ui/icons/Receipt";
 import PeopleMenu from "./PeopleMenu";
 import CategoriesMenu from "./CategoriesMenu";
+import ArrowDropDownIcon from "@material-ui/icons/ArrowDropDown";
+import avater from "../imgs/avater.jpeg";
+import english from "../imgs/en.png";
+import ExitToAppIcon from "@material-ui/icons/ExitToApp";
 
 const styles = (theme: Theme) =>
   createStyles({
@@ -41,6 +45,29 @@ const styles = (theme: Theme) =>
       paddingRight: theme.spacing(7.5),
       height: 40,
       width: 100,
+    },
+    rightPartOfHeader: {
+      display: "flex",
+      alignItems: "center",
+      marginLeft: theme.spacing(12),
+    },
+    languageImg: {
+      maxHeight: 35,
+      maxWidth: 35,
+      paddingRight: theme.spacing(1),
+    },
+    userInfo: {
+      display: "flex",
+      alignItems: "center",
+      color: theme.palette.text.secondary,
+    },
+    avatar: {
+      width: theme.spacing(4),
+      height: theme.spacing(4),
+      marginRight: theme.spacing(1),
+    },
+    colorButton: {
+      color: theme.palette.text.secondary,
     },
   });
 
@@ -111,6 +138,24 @@ const HeaderComponent: React.FunctionComponent<WithStyles<typeof styles>> = (
           </Link>
         </Box>
       </Toolbar>
+      <Box className={classes.rightPartOfHeader}>
+        <Box className={classes.userInfo}>
+          <Avatar
+            alt="Profile picture"
+            src={avater}
+            className={classes.avatar}
+          />
+          <Typography>userName </Typography>
+          {/* i will change it to the userName  */}
+        </Box>
+        <Button href="#" className={classes.colorButton}>
+          <img src={english} className={classes.languageImg} alt="english" />
+          <ArrowDropDownIcon fontSize="small" />
+        </Button>
+        <Button className={classes.colorButton}>
+          <ExitToAppIcon fontSize="default" />
+        </Button>
+      </Box>
     </AppBar>
   );
 };
